effe1ea536c136eb0c7beafa74ef82c938bfdd15
YuyouWu <yuyouwu5@gmail.com>
2020-12-30 20:50:38 -0800

    Minor style change for NavBar

--- components/NavBar.js
+++ components/NavBar.js
@@ -2,24 +2,29 @@ import { useState } from 'react';
 import { Menu, Segment } from 'semantic-ui-react';
 
 export default function NavBar() {
-  const [activeItem, setActiveItem] = useState('home');
+    const [activeItem, setActiveItem] = useState('home');
 
-  return (
-    <div>
-      <Segment inverted>
-        <Menu inverted pointing secondary>
-          <Menu.Item
-            name='home'
-            active={activeItem === 'home'}
-            onClick={() => setActiveItem('home')}
-          />
-          <Menu.Item
-            name='about'
-            active={activeItem === 'about'}
-            onClick={() => setActiveItem('about')}
-          />
-        </Menu>
-      </Segment>
-    </div>
-  )
+    return (
+        <div>
+            <Segment
+                inverted
+                style={{
+                    borderRadius: 0
+                }}
+            >
+                <Menu inverted pointing secondary>
+                    <Menu.Item
+                        name='home'
+                        active={activeItem === 'home'}
+                        onClick={() => setActiveItem('home')}
+                    />
+                    <Menu.Item
+                        name='about'
+                        active={activeItem === 'about'}
+                        onClick={() => setActiveItem('about')}
+                    />
+                </Menu>
+            </Segment>
+        </div>
+    )
 }
